73dce4ef2879a8c75118127d2b4cad10ba9f2015
JuanCarlosxd01 <juan_alcerro@unitec.edu>
2026-08-01 09:38:25 -0600

    .

--- src/laboratorio2progra2/Empleado.java
+++ src/laboratorio2progra2/Empleado.java
@@ -1,7 +1,34 @@
-]
+
 package laboratorio2progra2;
 
+import java.io.File;
+import java.util.Calendar;
 
+/**
+ *
+ * @author Ian Suazo Palao
+ */
 public class Empleado {
+    protected String codigo;
+    protected String nombre;
+    protected Calendar contrato;
+    protected double salario_b;
+    protected double horas;
+    protected File foto;
     
+    public Empleado(String codigo, String nombre, double salario_b, Calendar contrato, File fotoArchivo){
+        this.codigo=codigo;
+        this.nombre=nombre;
+        this.salario_b=salario_b;
+        
+        if (contrato!=null){
+            this.contrato=contrato;
+        }
+        else{
+            this.contrato=Calendar.getInstance();
+        }
+        
+        this.horas=0.0;
+        this.foto=fotoArchivo;
+    }
 }
